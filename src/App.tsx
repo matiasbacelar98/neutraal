0fc81421aefedef9fc2d3156fe291cbd61d3dc48
Matias Bacelar <matiasbacelar@gmail.com>
2023-07-04 19:34:05 -0300

    React router base config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
+import Router from './Router';
+
 export default function App() {
-  return (
-    <main className="font-circularStd bg-bg">
-      <h1 className="text-2xl font-bold text-text">App...</h1>
-      <p className="text-xl font-book text-text">Heading</p>
-      <p className="text-lg font-book text-text">Heading</p>
-      <p className="text-md font-book text-text">Heading</p>
-      <p className="text-base font-book text-text">Heading</p>
-      <p className="text-sm font-book text-text">Heading</p>
-      <p className="text-sm font-medium text-text">Heading</p>
-    </main>
-  );
+  return <Router />;
 }
---
+++ src/Router.tsx
@@ -0,0 +1,35 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/sign-up" element={<div>Create account</div>} />
+      <Route path="/sign-in" element={<div>Log in</div>} />
+      <Route path="/account/*" element={<MultiStepRouter />} />
+      <Route path="*" element={<div>Not found</div>} />
+    </Routes>
+  );
+}
+
+function MultiStepRouter() {
+  return (
+    <Routes>
+      <Route
+        path="/multistep-form"
+        element={
+          <div>
+            <p>Shared multistep component</p>
+            <Outlet />
+          </div>
+        }
+      >
+        <Route path="details" element={<div>Details component</div>} />
+        <Route path="plan" element={<div>Plan component</div>} />
+        <Route path="confirm" element={<div>Confirm component</div>} />
+      </Route>
+
+      <Route path="/success" element={<div>Process completed</div>} />
+      <Route path="*" element={<div>Not found</div>} />
+    </Routes>
+  );
+}
