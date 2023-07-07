49956843b79a4cb9139d7f60de4f520819a81b78
Matias Bacelar <matiasbacelar@gmail.com>
2023-07-07 17:34:12 -0300

    Path aliases configuration

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,11 +1,16 @@
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SignUp from '@pages/SignUp';
+import SignIn from '@pages/SignIn';
+import MultiStepForm from '@pages/MultiStepForm';
+import Success from '@pages/Success';
+import NotFound from '@pages/NotFound';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/sign-up" />} />
-      <Route path="/sign-up" element={<div>Create account</div>} />
-      <Route path="/sign-in" element={<div>Log in</div>} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path="/account/*" element={<MultiStepRouter />} />
       <Route path="*" element={<div>Not found</div>} />
     </Routes>
@@ -15,22 +20,14 @@ export default function Router() {
 function MultiStepRouter() {
   return (
     <Routes>
-      <Route
-        path="/multistep-form"
-        element={
-          <div>
-            <p>Shared multistep component</p>
-            <Outlet />
-          </div>
-        }
-      >
+      <Route path="/multistep-form" element={<MultiStepForm />}>
         <Route path="details" element={<div>Details component</div>} />
         <Route path="plan" element={<div>Plan component</div>} />
         <Route path="confirm" element={<div>Confirm component</div>} />
       </Route>
 
-      <Route path="/success" element={<div>Process completed</div>} />
-      <Route path="*" element={<div>Not found</div>} />
+      <Route path="/success" element={<Success />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
